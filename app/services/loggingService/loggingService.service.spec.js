50d0ca3706c46e7055832b45a2983c3679524b65
mukkashwin <ashwin2722@gmail.com>
2017-02-08 13:37:49 -0600

    Update loggingService.service.spec.js

--- app/services/loggingService/loggingService.service.spec.js
+++ app/services/loggingService/loggingService.service.spec.js
@@ -1,9 +1,52 @@
 'use strict';
 describe('loggingService', function () {
 
-  it('TBD',function() {
+  	var $injector, loggingService, API_URLS, $log, $window;
+	var $location; // private var lazy loaded to prevent circular dependency
+    // cannot inject these until they are needed because of circular dependency issues
+    var $http;     // eslint-disable-line init-declarations
+	var pendingLogs = [];
 
-  });
+	beforeEach(module('myApp.services'));
+
+   beforeEach(inject(function ($injector, _loggingService_, $location, $http, $timeout, _$rootScope_){
+	    $injector = $injector;
+	    loggingService = _loggingService_;
+	    $location = $location;
+	    $http = $http;
+	    API_URLS = API_URLS;
+	    $log = $log;
+	    $window = $window;
+	    $timeout = $timeout;
+	    $scope = $rootScope.$new();
+  }));
+
+   it('should test $location service', function(){
+	  	var result =loggingService.getLocationService();
+	  	expect(result).toBe($location);
+	});
+
+	it('should test $timeout, $http service', function(){
+	  	loggingService.initializeCircularDependencies();
+	  	expect($scope.$timeout).toEqual($timeout);
+	  	expect($scope.$http).toEqual($http);
+	});
+
+	it('should test debug method', function(){
+	  	loggingService.debug("Success","one");
+	  	expect($scope.logType).toEqual("debug");
+	});
+
+	it('should test error method', function(){
+	  	loggingService.error("failure");
+	  	expect($scope.logType).toEqual("error");
+	});
+
+	it('should test exceptionHandler method', function(){
+	  	loggingService.exceptionHandler({"message":"exception","stack":"102"},"main");
+	  	expect($scope.logType).toEqual("exception");
+	  	expect($scope.data.message).toEqual("exception");
+	});
 
 });
 
